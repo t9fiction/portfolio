e198054f2755061e55268dcf7fabdd16eb44bd14
t9fiction <sohail.sohailishaq@gmail.com>
2025-02-06 02:44:17 +0500

    added ignore for the error in Globe

--- components/ui/Globe.tsx
+++ components/ui/Globe.tsx
@@ -2,6 +2,7 @@
 import { useEffect, useRef, useState } from "react";
 import { Color, Scene, Fog, PerspectiveCamera, Vector3 } from "three";
 import ThreeGlobe from "three-globe";
+// @ts-ignore
 import { useThree, Object3DNode, Canvas, extend } from "@react-three/fiber";
 import { OrbitControls } from "@react-three/drei";
 import countries from "@/data/globe.json";
