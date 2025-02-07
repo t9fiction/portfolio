f5850c194133fa820f5e2a4edc5d1920564b1deb
t9fiction <sohail.sohailishaq@gmail.com>
2025-02-08 02:47:49 +0500

    css style added to bullets

--- components/RecentProjects.tsx
+++ components/RecentProjects.tsx
@@ -6,10 +6,7 @@ import { projects } from "@/data";
 import { PinContainer } from "./ui/Pin";
 import Link from "next/link";
 import { Swiper, SwiperSlide } from "swiper/react";
-import {
-  Navigation,
-  Pagination,
-} from "swiper/modules";
+import { Navigation, Pagination } from "swiper/modules";
 import "swiper/css";
 // import "swiper/css/navigation";
 import "swiper/css/pagination";
@@ -22,86 +19,82 @@ const RecentProjects = () => {
         <span className="text-purple">recent projects</span>
       </h1>
 
-        <Swiper
-          spaceBetween={30}
-          slidesPerView={3}
-          loop={true}
-          autoplay={{ delay: 2000 }}
-          modules={[Navigation, Pagination]}
-          // navigation
-          // mousewheel
-          pagination={{ clickable: true }}
-          breakpoints={{
-            320: { slidesPerView: 1 },  // Mobile (1 card)
-            640: { slidesPerView: 2 },  // Small screens (2 cards)
-            1024: { slidesPerView: 3 }, // Large screens (3 cards)
-            1280: { slidesPerView: 4 }, // Extra large screens (4 cards)
-          }}
-        >
-          {projects.map((item) => (
-            <SwiperSlide>
-              <div
-                className="lg:min-h-[32.5rem] h-[25rem] flex items-center justify-center cursor-grab sm:w-96 w-[80vw]"
-                key={item.id}
-              >
-                <PinContainer title={item.title} href={item.link}>
-                  <div className="relative flex items-center justify-center sm:w-80 w-[80vw] overflow-hidden h-[16vh] lg:h-[30vh]">
-                    <div
-                      className="relative w-full h-full overflow-hidden lg:rounded-3xl "
-                      style={{ backgroundColor: "#13162D" }}
-                    >
-                      <img src="/bg.png" alt="bgimg" />
-                    </div>
-                    <img
-                      src={item.img}
-                      alt="cover"
-                      className="z-10 absolute top-0 object-cover p-2"
-                    />
-                  </div>
-
-                  <h1 className="font-bold lg:text-2xl md:text-xl text-base line-clamp-1">
-                    {item.title}
-                  </h1>
-
-                  <p
-                    className="lg:text-xl lg:font-normal font-light text-sm line-clamp-2"
-                    style={{
-                      color: "#BEC1DD",
-                      margin: "1vh 0",
-                    }}
+      <Swiper
+        spaceBetween={30}
+        loop={true}
+        autoplay={{ delay: 2000 }}
+        modules={[Navigation, Pagination]}
+        // navigation
+        // mousewheel
+        pagination={{ clickable: true }}
+        breakpoints={{
+          320: { slidesPerView: 1 },
+          640: { slidesPerView: 2 },
+          1024: { slidesPerView: 3 },
+          1536: { slidesPerView: 4 },
+        }}
+      >
+        {projects.map((item) => (
+          <SwiperSlide key={item.id}>
+            <div className="lg:min-h-[32.5rem] h-[25rem] flex items-center justify-center cursor-grab sm:w-96 w-[80vw]">
+              <PinContainer title={item.title} href={item.link}>
+                <div className="relative flex items-center justify-center sm:w-80 w-[80vw] overflow-hidden h-[16vh] lg:h-[30vh]">
+                  <div
+                    className="relative w-full h-full overflow-hidden lg:rounded-3xl "
+                    style={{ backgroundColor: "#13162D" }}
                   >
-                    {item.des}
-                  </p>
-
-                  <div className="flex items-center justify-between mt-7 mb-3">
-                    {/* Icons */}
-                    <div className="flex items-center">
-                      {item.iconLists.map((icon, index) => (
-                        <div
-                          key={index}
-                          className="border border-white/[.2] rounded-full bg-black lg:w-10 lg:h-10 w-8 h-8 flex justify-center items-center"
-                          style={{
-                            transform: `translateX(-${5 * index + 2}px)`,
-                          }}
-                        >
-                          <img src={icon} alt="icon5" className="p-2" />
-                        </div>
-                      ))}
-                    </div>
-
-                    <div className="flex justify-center items-center hover:underline cursor-pointer">
-                      <p className="flex lg:text-xl md:text-xs text-sm  text-purple">
-                        <Link href={item.link || "/"}>Live Site</Link>
-                      </p>
-                      <FaLocationArrow className="ms-3" color="#CBACF9" />
-                    </div>
+                    <img src="/bg.png" alt="bgimg" />
                   </div>
-                </PinContainer>
-              </div>
-            </SwiperSlide>
-          ))}
-        </Swiper>
-      </div>
+                  <img
+                    src={item.img}
+                    alt="cover"
+                    className="z-10 absolute top-0 object-cover p-2"
+                  />
+                </div>
+
+                <h1 className="font-bold lg:text-2xl md:text-xl text-base line-clamp-1">
+                  {item.title}
+                </h1>
+
+                <p
+                  className="lg:text-xl lg:font-normal font-light text-sm line-clamp-2"
+                  style={{
+                    color: "#BEC1DD",
+                    margin: "1vh 0",
+                  }}
+                >
+                  {item.des}
+                </p>
+
+                <div className="flex items-center justify-between mt-7 mb-3">
+                  {/* Icons */}
+                  <div className="flex items-center">
+                    {item.iconLists.map((icon, index) => (
+                      <div
+                        key={index}
+                        className="border border-white/[.2] rounded-full bg-black lg:w-10 lg:h-10 w-8 h-8 flex justify-center items-center"
+                        style={{
+                          transform: `translateX(-${5 * index + 2}px)`,
+                        }}
+                      >
+                        <img src={icon} alt="icon5" className="p-2" />
+                      </div>
+                    ))}
+                  </div>
+
+                  <div className="flex justify-center items-center hover:underline cursor-pointer">
+                    <p className="flex lg:text-xl md:text-xs text-sm  text-purple">
+                      <Link href={item.link || "/"}>Live Site</Link>
+                    </p>
+                    <FaLocationArrow className="ms-3" color="#CBACF9" />
+                  </div>
+                </div>
+              </PinContainer>
+            </div>
+          </SwiperSlide>
+        ))}
+      </Swiper>
+    </div>
   );
 };
 
