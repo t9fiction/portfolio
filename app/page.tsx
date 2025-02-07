f45f68acb3d7713e943f6fa088b13561c2ee8aec
t9fiction <sohail.sohailishaq@gmail.com>
2025-02-08 00:08:17 +0500

    stars added

--- app/page.tsx
+++ app/page.tsx
@@ -8,11 +8,18 @@ import Hero from "@/components/Hero";
 import RecentProjects from "@/components/RecentProjects";
 import { FloatingNav } from "@/components/ui/FloatingNavbar";
 import { navItems } from "@/data";
+import Starfield from 'react-starfield';
 
 export default function Home() {
   return (
     <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
     <div className="max-w-7xl w-full">
+       <Starfield
+              starCount={1000}
+              starColor={[255, 255, 255]}
+              speedFactor={0.05}
+              backgroundColor="black"
+            />
       <FloatingNav navItems={navItems} />
       <Hero />
       <Grid />
