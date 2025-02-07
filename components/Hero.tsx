1b414449f9fb745f36c71efa19e4e10a989955a3
t9fiction <sohail.sohailishaq@gmail.com>
2025-02-07 18:44:02 +0500

    home section personalized

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -4,7 +4,6 @@ import { TextGenerateEffect } from "./ui/TextGenerateEffect";
 import MagicButton from "./MagicButton";
 import { FaLocationArrow } from "react-icons/fa6";
 
-
 const Hero = () => {
   return (
     <div className="pb-20 pt-36">
@@ -44,16 +43,17 @@ const Hero = () => {
       <div className="flex justify-center relative my-20 z-10">
         <div className="max-w-[89vw] md:max-w-2xl lg:max-w-[60vw] flex flex-col items-center justify-center">
           <p className="uppercase tracking-widest text-xs text-center text-blue-100 max-w-80">
-            Dynamic Web Magic with Next.js
+            Blockchain | Smart Contracts | Web3
           </p>
 
           <TextGenerateEffect
-            words="Transforming Concepts into Seamless User Experiences"
+            words="Crafting the Web3 Evolution, One Block at a Time"
             className="text-center text-[40px] md:text-5xl lg:text-6xl"
           />
 
-          <p className="text-center md:tracking-wider mb-4 text-sm md:text-lg lg:text-2xl">
-            Hi! I&apos;m Sohail, a Blockchain Developer based in Pakistan.
+          <p className="text-center md:tracking-wider mb-4 text-sm md:text-lg lg:text-2xl text-white font-thin">
+            Blockchain Engineer, Web3 Explorer, Future-Driven Technologist Hi,
+            I&apos;m Sohail!
           </p>
 
           <a href="#about">
