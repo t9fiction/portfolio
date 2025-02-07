8245a21dedb5b1d6c98e048b2980bc857fcc6806
t9fiction <sohail.sohailishaq@gmail.com>
2025-02-07 17:27:36 +0500

    added unknown in Globe

--- components/ui/Globe.tsx
+++ components/ui/Globe.tsx
@@ -174,7 +174,7 @@ export function Globe({ globeConfig, data }: WorldProps) {
       .arcStartLng((d) => (d as { startLng: number }).startLng * 1)
       .arcEndLat((d) => (d as { endLat: number }).endLat * 1)
       .arcEndLng((d) => (d as { endLng: number }).endLng * 1)
-      .arcColor((e: any) => (e as { color: string }).color)
+      .arcColor((e: unknown) => (e as { color: string }).color)
       .arcAltitude((e) => {
         return (e as { arcAlt: number }).arcAlt * 1;
       })
