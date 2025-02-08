1cee979dba23507414e89cb2308798fa78e9a5b4
t9fiction <sohail.sohailishaq@gmail.com>
2025-02-09 04:51:15 +0500

    margin reduced in footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -6,7 +6,7 @@ import Link from "next/link";
 
 const Footer = () => {
   return (
-    <footer className="w-full mb-[100px] md:mb-5 pb-4" id="contact">
+    <footer className="w-full mb-[20px] md:mb-5 pb-4" id="contact">
       {/* background grid */}
       {/* <div className="w-full absolute left-0 -bottom-72 min-h-96">
         <img
