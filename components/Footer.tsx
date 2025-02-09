4c302d7b63f1c59e699ac7617a8392875255578c
t9fiction <sohail.sohailishaq@gmail.com>
2025-02-09 23:53:46 +0500

    footer updated

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -42,15 +42,13 @@ const Footer = () => {
       <div className="flex mt-16 md:flex-row flex-col justify-between items-center">
         <p className="md:text-base text-sm md:font-extralight font-thin">
           © 2025 Sohail Ishaque. Feel free to use and share with proper credit.
-
-
         </p>
 
         <div className="flex items-center md:gap-3 gap-6">
           {socialMedia.map((info) => (
             <div
               key={info.id}
-              className="w-10 h-10 cursor-pointer flex justify-center items-center backdrop-filter backdrop-blur-lg saturate-180 bg-opacity-75 bg-black-200 rounded-lg border border-black-300"
+              className="z-10 w-10 h-10 cursor-pointer flex justify-center items-center backdrop-filter backdrop-blur-lg saturate-180 bg-opacity-75 bg-black-200 rounded-lg border border-black-300"
             >
               <Link href={info.link} target="_blank">
                 <img src={info.img} alt="icons" width={20} height={20} />
